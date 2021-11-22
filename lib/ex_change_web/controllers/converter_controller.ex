6c161c3d163462a23aa12bde18651b536745cda5
Gabriel Carraro <gacarraro@gmail.com>
2021-11-21 21:31:45 -0300

    matching struct

--- lib/ex_change_web/controllers/converter_controller.ex
+++ lib/ex_change_web/controllers/converter_controller.ex
@@ -1,9 +1,10 @@
 defmodule ExChangeWeb.ConverterController do
   use ExChangeWeb, :controller
+  alias ExChange.ConversionInfo
   action_fallback(ExChangeWeb.FallbackController)
 
   def convert(conn, params) do
-    with {:ok, conversion_info} <- ExChange.convert(params) do
+    with {:ok, %ConversionInfo{} = conversion_info} <- ExChange.convert(params) do
       render(conn, "convert.json", converter: conversion_info)
     end
   end
--- lib/ex_change_web/views/converter_view.ex
+++ lib/ex_change_web/views/converter_view.ex
@@ -1,11 +1,12 @@
 defmodule ExChangeWeb.ConverterView do
   use ExChangeWeb, :view
+  alias ExChange.ConversionInfo
 
-  def render("convert.json", %{converter: conversion_info}) do
+  def render("convert.json", %{converter: %ConversionInfo{} = conversion_info}) do
     %{data: render_one(conversion_info, ExChangeWeb.ConverterView, "conversion_info.json")}
   end
 
-  def render("conversion_info.json", %{converter: conversion_info}) do
+  def render("conversion_info.json", %{converter: %ConversionInfo{} = conversion_info}) do
     %{
       currency: Map.get(conversion_info, :currency),
       amount: Map.get(conversion_info, :amount),
